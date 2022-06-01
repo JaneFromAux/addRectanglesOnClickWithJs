f795b9ebb6dfeb6743334cc6da7d192985d5812a
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-06-01 14:36:44 +0200

    fertig, aber wirklich

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,13 @@
 
 let button = document.querySelector('#button');
-
 let wicki = document.querySelector('.umwickeln');
 let counter = 0;
 
 button.addEventListener('click', (ele) => {
     let divi = document.createElement('div');
+    divi.classList.add('rechteck');
     if (counter == 0 || counter % 10 == 0) {
         divi.classList.add('weiss');
-    } else {
-        divi.classList.add('rechteck');
     }
     let counti = document.createTextNode(counter);
     divi.appendChild(counti);
